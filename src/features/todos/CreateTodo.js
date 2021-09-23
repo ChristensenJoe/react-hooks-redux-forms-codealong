5107d5b54c755c9012aa04edcdb2248d0b73d3b9
Joe Christensen <joechristensenwork@gmail.com>
2021-09-23 08:51:24 -0600

    commit tree ef1a50e282838621f858337fb2bc04e908854980

--- src/features/todos/CreateTodo.js
+++ src/features/todos/CreateTodo.js
@@ -1,7 +1,34 @@
 import React from "react";
 
+import {useDispatch} from 'react-redux'
+import { todoAdded } from './todosSlice'
+
 function CreateTodo() {
-  return <div>Create Todo Component</div>;
+  const dispatch = useDispatch();
+  const [text,setText] = useState("")
+
+  function handleChange(e) {
+    setText(text);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    dispatch(todoAdded(text))
+  }
+
+  return (
+    <div>
+      <form
+        onSubmit={handleSubmit}
+      >
+        <p>
+          <label>add todo</label>
+          <input value={text} type="text" onChange={handleChange}/>
+        </p>
+        <input type="submit" />
+      </form>
+    </div>
+  )
 }
 
 export default CreateTodo;
